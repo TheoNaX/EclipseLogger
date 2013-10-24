0accbd45991373b54c6d86b89fb47ed110bfa217
TheoNaX <t.caban@gmailcom>
2013-10-25 00:33:02 +0200

    improved logging, file lines changes

--- src/eclipselogger/logging/ConsoleActionLogger.java
+++ src/eclipselogger/logging/ConsoleActionLogger.java
@@ -10,6 +10,7 @@ import eclipselogger.events.actions.OpenNewFileAction;
 import eclipselogger.events.actions.RefactorFileAction;
 import eclipselogger.events.actions.RefactorPackageAction;
 import eclipselogger.events.actions.SaveFileAction;
+import eclipselogger.events.actions.SwitchToFileAction;
 
 public class ConsoleActionLogger implements EclipseActiontLogIF {
 
@@ -42,15 +43,25 @@ public class ConsoleActionLogger implements EclipseActiontLogIF {
 		if (action instanceof DeletePackageAction) {
 			logPackageDeleted(action);
 		}
-		
+		if (action instanceof SwitchToFileAction) {
+			logSwitchToFileAction(action);
+		}
 	}
 	
+
 	private void logDeleteFileAction(EclipseAction action) {
 		DeleteFileAction deleteAction = (DeleteFileAction) action;
 		System.out.println("============================================");
 		System.out.println("File deleted: " + deleteAction.getDeletedFile().getProjectRelativePath());
-		//System.out.println("Working time: " + deleteAction.getWorkingFile().getWorkingTime() + " ms");
 		System.out.println("In same package: " + deleteAction.isSamePackage() + ", in same project: " + deleteAction.isSameProject() + ", same file type: " + deleteAction.isSameFileType());
+		
+		if (deleteAction.getWorkingFile() != null) {
+			System.out.println("Working time: "
+					+ deleteAction.getWorkingFile().getWorkingTime() + " ms");
+			System.out.println(deleteAction.getWorkingFile().getFileChanges());
+		} else {
+			System.out.println("Deleted file was not worked with!!!");
+		}
 		System.out.println("============================================");
 	}
 
@@ -58,8 +69,12 @@ public class ConsoleActionLogger implements EclipseActiontLogIF {
 		CloseFileAction closeAction = (CloseFileAction) action;
 		System.out.println("============================================");
 		System.out.println("File closed: " + closeAction.getClosedFile().getProjectRelativePath());
-		System.out.println("Working time: " + closeAction.getWorkingFile().getWorkingTime() + " ms");
 		System.out.println("In same package: " + closeAction.closedInSamePackage() + ", in same project: " + closeAction.closedInSameProject() + ", same file type: " + closeAction.isTheSameTypeAsPreviuos());
+		
+		if (closeAction.getWorkingFile() != null) {
+			System.out.println("Working time: " + closeAction.getWorkingFile().getWorkingTime() + " ms");
+			System.out.println(closeAction.getWorkingFile().getFileChanges());
+		}
 		System.out.println("============================================");
 	}
 	
@@ -68,6 +83,14 @@ public class ConsoleActionLogger implements EclipseActiontLogIF {
 		System.out.println("============================================");
 		System.out.println("File opened: " + openAction.getOpenedFile().getProjectRelativePath());
 		System.out.println("In same package: " + openAction.openedInSamePackage() + ", in same project: " + openAction.openedInSameProject() + ", same file type: " + openAction.isTheSameTypeAsPreviuos());
+		System.out.println("============================================");
+	}
+	
+	private void logSwitchToFileAction(EclipseAction action) {
+		SwitchToFileAction switchAction = (SwitchToFileAction) action;
+		System.out.println("============================================");
+		System.out.println("File opened: " + switchAction.getOpenedFile().getProjectRelativePath());
+		System.out.println("In same package: " + switchAction.openedInSamePackage() + ", in same project: " + switchAction.openedInSameProject() + ", same file type: " + switchAction.isTheSameTypeAsPreviuos());
 		System.out.println("============================================");
 	}
 	
